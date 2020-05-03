2e2477bc67bd887f5e7300ffc4e5af512c06fa3f
Rob Dyson <rdyson@hotmail.com>
2020-05-03 18:25:27 -0500

    Added Mute your microphone post, first one

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,16 @@ module.exports = {
         },
         gatsbyRemarkPlugins: [
           {
+            resolve: 'gatsby-remark-embed-video',
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+            },
+          },
+          {
             resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 1536,
@@ -129,6 +139,7 @@ module.exports = {
         ],
       },
     },
+
     'gatsby-transformer-json',
     'gatsby-transformer-sharp',
     'gatsby-plugin-catch-links',
